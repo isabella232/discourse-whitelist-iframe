a48ec4a70162d06b19c1dc49f750f5bbf316a5d6
Margaret Rosas <margaret@looker.com>
2016-01-04 18:44:56 -0800

    adding urls

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -2,5 +2,10 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learn\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learnbeta\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/meta\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/metanew\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/looker\.com\/.+/i);
 })();
 

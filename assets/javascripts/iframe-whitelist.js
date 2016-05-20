831954faa825b23e6f2a7f3be5ee6d2d598fad9d
Phil Sharfstein <phil@looker.com>
2016-05-20 10:56:47 -0700

    add docs.google.com to whitelist

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -4,6 +4,7 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learn\.looker\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learnbeta\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/docs\.google\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/meta\.looker\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/metanew\.looker\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/looker\.com\/.+/i);

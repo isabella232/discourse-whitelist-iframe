406b61aa64a5dc658c5e9ccfd3023da06f8c2bd8
Phil Sharfstein <phil@looker.com>
2016-08-25 00:20:24 -0700

    re-add old stuff

---
+++ assets/javascripts/iframe-whitelist.js
@@ -0,0 +1,16 @@
+(function() {
+  if (Discourse.dialect_deprecated) { return; }
+   
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learn\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/learnbeta\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/docs\.google\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/meta\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/metanew\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/.*\.looker\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/raw\.githubusercontent\.com\/.+/i);
+})();
+
